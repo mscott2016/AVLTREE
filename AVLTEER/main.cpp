8eb7486f35c0aa53da7961cc40e36cccdf4b214a
mscott2016 <mscott2016@my.fit.edu>
2018-03-01 11:18:27 -0500

    Initial Commit

---
+++ AVLTEER/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AVLTEER
+//
+//  Created by Marley on 3/1/18.
+//  Copyright © 2018 Mar Z Mar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
